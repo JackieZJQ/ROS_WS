d137cc6ac5f18451ebeb102e5a35bb24403c68c4
Jackie <zhangiaii97@gmail.com>
2023-09-14 20:25:20 +0800

    write forward

--- src/cam_module/include/camLane.hpp
+++ src/cam_module/include/camLane.hpp
@@ -4,13 +4,16 @@
 #include "rclcpp/rclcpp.hpp"
 #include "opencv2/opencv.hpp"
 
-#include "string.h"
-
 #include "cv_bridge/cv_bridge.h"
 #include "sensor_msgs/msg/image.hpp"
 
 #include <torch/script.h> 
 #include <torch/torch.h>
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility> 
 
 using std::placeholders::_1;
 class camLane : public rclcpp::Node {
@@ -20,7 +23,9 @@ public:
 
     void shortImagecallback(const sensor_msgs::msg::Image::SharedPtr image);
 
-    void pred2coords();
+    void forward(const cv::Mat &_image);
+
+    std::vector<std::vector<std::pair<int, int>>> pred2coords(const c10::impl::GenericDict &pred);
 
 private:
     // rclcpp::callback_group::CallbackGroup::SharedPtr shortImagecallbackGroup;
@@ -28,9 +33,29 @@ private:
 
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr shortImagesub;
 
+    // 原图片参数
+    int ori_img_w = 640;
+    int ori_img_h = 480;
+
+    // 模型训练参数
+    int train_width= 1600;
+    int train_height= 320;
+    
+    // 裁切比
+    float crop_ratio = 0.6;
+
+    // cut_height = (int)(train_height * (1 - crop_ratio))
+    int cut_height = 128;
+
+    int num_row= 72;
+    int num_col= 81;
+
+    // if cuda available
+    bool cudaAvailable;
+
+    
 protected:
     torch::jit::script::Module module;
-
 };
 
 #endif // CAMLANE_HPP_
--- src/cam_module/src/camLane.cpp
+++ src/cam_module/src/camLane.cpp
@@ -10,7 +10,7 @@ camLane::camLane(const std::string &name):Node(name) {
 
     cv::namedWindow("Test Window", 1);
 
-    bool cudaAvailable = torch::cuda::is_available();
+    cudaAvailable = torch::cuda::is_available();
     if (cudaAvailable) RCLCPP_INFO(this->get_logger(), "---CUDA available! Predicting on GPU---");
 
     // 设置推理设备
@@ -41,10 +41,9 @@ void camLane::shortImagecallback(const sensor_msgs::msg::Image::SharedPtr image)
         
         auto cvPtr = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::BGR8);
 
-        forward(cvPtr->image);
+        camLane::forward(cvPtr->image);
 
-        cv::imshow("Test Window", cvPtr->image);
-        cv::waitKey(2);
+
 
     } catch (cv_bridge::Exception& e) {
       
@@ -53,18 +52,95 @@ void camLane::shortImagecallback(const sensor_msgs::msg::Image::SharedPtr image)
     }
 }
 
-void camLane::forward(const cv::InputArray &_image) {
+void camLane::forward(const cv::Mat &_image) {
 
     if (_image.empty()) return;
 
-    cv::Mat image = _image.getMat();
+    // 设置推理设备
+    torch::DeviceType device_type;
+    
+    if (cudaAvailable) device_type = torch::kCUDA;
+    else device_type = torch::kCPU;
 
-    std::cout << image.size() << std::endl;
+    torch::Device device(device_type);
 
+    // 裁剪图片
+    cv::Mat image = _image(cv::Range(cut_height, ori_img_h), cv::Range(0, ori_img_w));
 
+    // std::cout << image_src.size() << std::endl;
+
+    cv::resize(image, image, cv::Size(train_width, train_height), 0, 0, cv::INTER_CUBIC);
+
+    cv::Mat img;
+    image.convertTo(image, -1, 1.f/255.f, 0);
+
+    //TO-DO？
+    //Normalize
+    
+    std::vector<int64_t> dims = {train_width, train_height, 3, 1};
+    torch::Tensor img_var = torch::from_blob(image.data, dims, torch::kByte).to(device);
+    img_var = img_var.permute({3, 2, 0, 1});
+    img_var = img_var.to(torch::kFloat32);
+
+    std::vector<torch::jit::IValue> inputs = {img_var};
+    auto outputs = module.forward(inputs).toGenericDict();
+    
+    std::vector<std::vector<std::pair<int, int>>> coords;
+    coords = pred2coords(outputs);
+    
+
+    // std::cout << image.type() << std::endl;
+    // std::cout << image_dst.size() << std::endl;
+
+    cv::imshow("Test Window", image);
+    cv::waitKey(2);
 }
 
-void camLane::pred2coords() {
+std::vector<std::vector<std::pair<int, int>>> camLane::pred2coords(const c10::impl::GenericDict &preds) {
 
+    // toTensor
+    auto loc_row = preds.at("loc_row").toTensor();
+    auto loc_col = preds.at("loc_col").toTensor();
+    auto exist_row = preds.at("exist_row").toTensor();
+    auto exist_col = preds.at("exist_row").toTensor();
+
+    // batch_size_row, num_grid_row, num_cls_row, num_lane_row 
+    int batch_size_row =  loc_row.sizes()[0];
+    int num_grid_row = loc_row.sizes()[1]; 
+    int num_cls_row = loc_row.sizes()[2];
+    int num_lane_row = loc_row.sizes()[3];
+
+    // batch_size_col, num_grid_col, num_cls_col, num_lane_col
+    int batch_size_col = loc_col.sizes()[0];
+    int num_grid_col = loc_col.sizes()[1]; 
+    int num_cls_col = loc_col.sizes()[2];
+    int num_lane_col = loc_col.sizes()[3];
+
+    auto max_indices_row = loc_row.argmax(1);
+    auto valid_row = exist_row.argmax(1);
+
+    // std::cout << valid_row << std::endl;
+
+    auto max_indices_col = loc_col.argmax(1);
+    auto valid_col = exist_col.argmax(1);
+
+    // initlize result
+    std::vector<std::vector<std::pair<int, int>>> res;
+    int row_lane_idx[2] = {1, 2};
+    int col_lane_idx[2] = {0, 3};
+    
+    std::cout << valid_row.index({0, torch::indexing::Slice(torch::indexing::None, torch::indexing::None), 1}).sum() << std::endl;
+
+    // for (int i: row_lane_idx) {
+    //     std::vector<std::pair<int, int>> tmp;
+    //     if (valid_row[0, :, i].sum())
+
+    // }
+
+    // for (int i: col_lane_idx) {
+
+    // }
+
+    return res;
 }
 
